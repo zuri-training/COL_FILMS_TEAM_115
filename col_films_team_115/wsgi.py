9d0d4725e6534d3ab2179f0559b90e5815109a0e
devbobnwaka <nwakachibueze@gmail.com>
2022-07-30 12:40:35 +0100

    django initial setup with the environment variables

---
+++ col_films_team_115/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for col_films_team_115 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'col_films_team_115.settings')
+
+application = get_wsgi_application()
